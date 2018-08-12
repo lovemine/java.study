45dcc8b290b5068f32ffbc45a6fb3098b9343ed0
knamkim <knamkim@gmail.com>
2018-08-12 22:48:56 +0900

    add

--- src/org/windfree/util/ArrayLoateUtil.java
+++ src/org/windfree/util/ArrayLoateUtil.java
@@ -12,7 +12,38 @@ public class ArrayLoateUtil {
 		}
 	}
 	
-	public static void rotate90Clockwise(int[][] arr) {
+	public static int[][] rotate90Clockwise(int[][] orgArr) {
+		//orgArray(row,col) --> rotatedArray(col, (rowCount -1 )- row) 
+		int rowCount = orgArr.length;
+		int colCount = orgArr[0].length;
 		
+		int[][] targetArr = new int[colCount][rowCount];
+		for(int i = 0; i < rowCount; i++) {
+			for(int j = 0 ; j < colCount; j++) {
+				targetArr[j][rowCount-1-i] =  orgArr[i][j];
+			}
+		}
+		
+		return targetArr;
+	}
+	
+	public static void main(String[] args) {
+		int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		printArray(arr);
+		System.out.println();
+		arr = rotate90Clockwise(arr);
+		printArray(arr);
+		
+		System.out.println();
+		arr = rotate90Clockwise(arr);
+		printArray(arr);
+		
+		System.out.println();
+		arr = rotate90Clockwise(arr);
+		printArray(arr);
+		
+		System.out.println();
+		arr = rotate90Clockwise(arr);
+		printArray(arr);
 	}
 }
